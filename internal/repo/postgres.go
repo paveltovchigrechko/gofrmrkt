3dfc022b244492919224069a93ecb956dd9568c1
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2026-09-04 11:01:02 +0600

    Import for file driver

--- internal/repo/postgres.go
+++ internal/repo/postgres.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
 )
